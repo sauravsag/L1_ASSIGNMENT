5595ab1777e44fe26fc9a95ae116db27cc64625f
sauravsag <146501978+sauravsag@users.noreply.github.com>
2023-10-01 09:53:27 +0530

    L1 SOLVED ASSIGNMENT ADDED HERE

---
+++ C++_ASSIGNMENT_L1 SOLUTION/QUESTION5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r = 10 , c;
+    cout<<"radius of circle = 10"<<endl;
+    c = 2*3.14*r;
+    cout<<"circumference of circle = ";
+    cout<<c;
+ }
